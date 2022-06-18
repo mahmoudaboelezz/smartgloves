43865117e0bfab597608c38d906ea66aa85cbeb4
aboelezz <mahmoudaboelezz51@gmail.com>
2022-06-18 04:43:15 +0200

    add reset

--- main/admin.py
+++ main/admin.py
@@ -1,24 +1,47 @@
 from django.contrib import admin
-from .models import HandGesture, RelatedWord, Emergency, Names , Battery
+from .models import HandGesture, RelatedWord, Emergency, Names , Battery, Reset
 # Register your models here.
-admin.site.register(HandGesture, admin.ModelAdmin)
+
 admin.site.register(Names)
+
+# add emergency and reset and related word to hand gesture stack 
+class ResetInline(admin.TabularInline):
+    model = Reset
+    extra = 1
+    
+class EmergencyInline(admin.TabularInline):
+    model = Emergency
+    extra = 1
 
 class RelatedWordInline(admin.StackedInline):
     model = RelatedWord
     extra = 1
 
-class EmergencyInline(admin.StackedInline):
-    model = Emergency
-    extra = 1
-    
+        
+
 class HandGestureAdmin(admin.ModelAdmin):
-    inlines = [RelatedWordInline, EmergencyInline]
+    inlines = [RelatedWordInline, EmergencyInline, ResetInline]
+    list_display = ('uuid', 'f1h1', 'f2h1', 'f3h1', 'f4h1', 'f5h1', 'f1h2', 'f2h2', 'f3h2', 'f4h2', 'f5h2')
+    
+    class Meta:
+        verbose_name_plural = "HandGesture"
+
+admin.site.register(HandGesture, HandGestureAdmin)
+admin.site.register(RelatedWord)
+admin.site.register(Emergency)
+admin.site.register(Reset)
+        
+
 
 class BatteryAdmin(admin.ModelAdmin):
     list_display = ('lhbattery', 'rhbattery')
     
-admin.site.register(RelatedWord)
-admin.site.register(Emergency)
+
+
 admin.site.register(Battery, BatteryAdmin)
     
+# change the admin title
+admin.site.site_header = 'SmartGloves Admin'
+admin.site.site_title = 'Aboelezz'
+admin.site.index_title = 'SmartGloves Project'
+
--- main/models.py
+++ main/models.py
@@ -6,53 +6,68 @@ class Names(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = "Team Members"
 
 class HandGesture(models.Model):
     # we have 10 resistors in both hands detected by the glove to be able to detect the gesture
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    f1h1 = models.FloatField(default=0)
-    f2h1 = models.FloatField(default=0)
-    f3h1 = models.FloatField(default=0)
-    f4h1 = models.FloatField(default=0)
-    f5h1 = models.FloatField(default=0)
-    f1h2 = models.FloatField(default=0)
-    f2h2 = models.FloatField(default=0)
-    f3h2 = models.FloatField(default=0)
-    f4h2 = models.FloatField(default=0)
-    f5h2 = models.FloatField(default=0)
+    f1h1 = models.IntegerField(default=0)
+    f2h1 = models.IntegerField(default=0)
+    f3h1 = models.IntegerField(default=0)
+    f4h1 = models.IntegerField(default=0)
+    f5h1 = models.IntegerField(default=0)
+    f1h2 = models.IntegerField(default=0)
+    f2h2 = models.IntegerField(default=0)
+    f3h2 = models.IntegerField(default=0)
+    f4h2 = models.IntegerField(default=0)
+    f5h2 = models.IntegerField(default=0)
     
 
     def __str__(self):
-        return self.uuid
+        # only show the first 5 digits of the uuid
+        return str(self.uuid)[:5]
     class Meta:
-        verbose_name_plural = "HandGesture"
+        verbose_name_plural = "Hand Gesture"
         
 class RelatedWord(models.Model):
-    word = models.CharField(max_length=100)
-    HandGesture = models.ForeignKey(HandGesture, on_delete=models.CASCADE)
+    # can be related to signle foreign key
+    word = models.CharField(max_length=200)
+    handgesture = models.OneToOneField(HandGesture, on_delete=models.CASCADE)
+
     
     def __str__(self):
         return self.word
     
     class Meta:
-        verbose_name_plural = "RelatedWord"
+        verbose_name_plural = "Related Word"
         
 class Emergency(models.Model):
-    emergency_number = models.CharField(max_length=100)
-    location_altitude = models.FloatField(default=0)
-    location_latitude = models.FloatField(default=0)
-    HandGesture = models.ForeignKey(HandGesture, on_delete=models.CASCADE)
+    send_location = models.BooleanField(default=False)
+    emergency_number = models.CharField(max_length=100, default='911')
+    # location_altitude = models.IntegerField(default=0)
+    # location_latitude = models.IntegerField(default=0)
+    HandGesture = models.OneToOneField(HandGesture, on_delete=models.CASCADE)
     def __str__(self):
-        return f'{self.emergency_number} {self.current_location}'
+        return f'{self.emergency_number} {self.HandGesture}'
     
     class Meta:
         verbose_name_plural = "Emergency"
         
 class Battery(models.Model):
-    lhbattery= models.FloatField(default=0)
-    rhbattery= models.FloatField(default=0)
+    lhbattery= models.IntegerField(default=0)
+    rhbattery= models.IntegerField(default=0)
     def __str__(self):
         return f'{self.lhbattery} {self.rhbattery}'
     
     class Meta:
         verbose_name_plural = "Battery"
+        
+class Reset(models.Model):
+    reset = models.BooleanField(default=False)
+    HandGesture = models.OneToOneField(HandGesture, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.reset}'
+    
+    class Meta:
+        verbose_name_plural = "Reset"
